01f4d6265cb1374a25cfba70b26d05c473db08df
Ale <alejandrina.roman@vtsd.com>
2022-10-03 10:32:29 -0400

    lab completed

--- Lab04avst.java
+++ Lab04avst.java
@@ -7,21 +7,30 @@ public class Lab04avst
 {
     public static void main(String[] args)
     {
-        System.out.println("Lab04a, Student Version\n");
+        System.out.println("Lab04a, Student 100 point Version\n");
 
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
-
-
-
-
-
-
-
-
-
-        System.out.println();
+        double numMonths  = numYears * 12;
+        double precent    = (annualRate / 100) / 12;
+        double numerator = precent * Math.pow(1 + precent,numMonths);
+        double denominator = (Math.pow(1 + precent, numMonths) - 1);
+        double MonthPay = (((numerator / denominator) * principal) * 100);
+        MonthPay = Math.round(MonthPay);
+        MonthPay /= 100;
+        double Tpay = (MonthPay * numMonths)* 100;
+        Tpay = Math.round(Tpay);
+        Tpay /= 100;
+        double Tintr = (Tpay - principal)* 100;
+        Tintr = Math.round(Tintr);
+        Tintr /= 100;
+        System.out.println("Principal:          $" + principal);
+        System.out.println("Annual Rate:        " + annualRate + "%");
+        System.out.println("Number of Years:    " + numYears);
+        System.out.println("Monthly Payment:    $" + MonthPay);
+        System.out.println("Total Payments:     $" + Tpay);
+        System.out.println("Total Interest:     $" + Tintr);
     }
 }
 
